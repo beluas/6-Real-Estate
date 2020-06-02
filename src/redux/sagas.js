6a1e5b9a4275a57f88167143e722cbe469b70f74
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-06-02 10:57:21 +0200

    Update sagas.js

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,7 +1,7 @@
 import { takeLatest, put, take } from "redux-saga/effects";
 
 export function* fetchAptSaga() {
-	const data = yield fetch("/real_estate.json");
+	const data = yield fetch("https://beluas.github.io/6-Real-Estate/real_estate.json");
 	const apts = yield data.json();
 
 	yield put({ type: "FETCH_ALL", payload: apts });
