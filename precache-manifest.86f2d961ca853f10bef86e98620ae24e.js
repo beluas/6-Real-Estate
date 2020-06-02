ced42c40bf648a6855e350d0b353e85612040839
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-06-02 10:42:29 +0200

    Updates

---
+++ precache-manifest.86f2d961ca853f10bef86e98620ae24e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc81c98ba5674bd95037a104a20c753",
+    "url": "/6-Real-Estate/index.html"
+  },
+  {
+    "revision": "74a0dd73f05a28e6b0aa",
+    "url": "/6-Real-Estate/static/css/main.b0102452.chunk.css"
+  },
+  {
+    "revision": "f504a8485e27c72a499a",
+    "url": "/6-Real-Estate/static/js/2.2d157eeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/6-Real-Estate/static/js/2.2d157eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a0dd73f05a28e6b0aa",
+    "url": "/6-Real-Estate/static/js/main.785e5d4d.chunk.js"
+  },
+  {
+    "revision": "72aa38ab157525bd0088",
+    "url": "/6-Real-Estate/static/js/runtime-main.bb2af2b0.js"
+  },
+  {
+    "revision": "59e0ff9c17aa366220af98f3e4557445",
+    "url": "/6-Real-Estate/static/media/picForm.59e0ff9c.svg"
+  }
+]);
